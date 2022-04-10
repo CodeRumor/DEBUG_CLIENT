707ec2cc783e9458cfa6350d269312803671def0
Ivan Richard Ugolole <30692799+ugolole@users.noreply.github.com>
2022-04-09 21:12:03 -0400

    Add correctly named component

---
+++ src/app/coppolog-detail/coppolog-detail.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-coppolog-detail',
+  templateUrl: './coppolog-detail.component.html',
+  styleUrls: ['./coppolog-detail.component.css']
+})
+export class CoppologDetailComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}

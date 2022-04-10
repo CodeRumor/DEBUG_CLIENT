85a84967e0fb104bcb871dab455859ea52f3a30c
Ivan Richard Ugolole <30692799+ugolole@users.noreply.github.com>
2022-04-09 21:02:02 -0400

    Update the app.modulle to contain a router module that will have the specified routing paths.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { RouterModule } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
@@ -14,7 +14,11 @@ import { CoppoLogComponent } from './coppo-log/coppo-log.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot([
+      {path: '', component: HomeComponent},
+      {path: 'coppolog', component:CoppoLogComponent}
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]

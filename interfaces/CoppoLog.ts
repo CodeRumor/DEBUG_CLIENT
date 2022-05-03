067ea1a0a8fb89dc4f0b487ed6c91093e3981aa8
Ivan Richard Ugolole <30692799+ugolole@users.noreply.github.com>
2022-05-03 17:33:28 -0400

    Adding a mock coppologs file that will be used initially to help in building the feature

--- interfaces/CoppoLog.ts
+++ interfaces/CoppoLog.ts
@@ -1,4 +1,4 @@
-// Defines the properties of the CoppoLog model.
+// An interface used to define properties of the CoppoLog model.
 export interface CoppoLog {
   Message : string;
   MessageTemplate : string;

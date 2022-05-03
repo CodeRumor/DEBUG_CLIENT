608d8096c5ee83e0e2bd5189be1617872db60870
Ivan Richard Ugolole <30692799+ugolole@users.noreply.github.com>
2022-05-03 18:57:54 -0400

    This interface was removed when the bug fix was being implemented it's now being added.

---
+++ src/app/interfaces/coppoLog.ts
@@ -0,0 +1,8 @@
+﻿// An interface used to define properties of the CoppoLog model.
+export interface CoppoLog {
+  Message : string;
+  MessageTemplate : string;
+  Level : string;
+  TimeStamp : string;
+  Exception : string;
+}

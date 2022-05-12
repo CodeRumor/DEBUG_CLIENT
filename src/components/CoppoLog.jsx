8b7451bd94bbad910e24da0e5869b6deef3263fd
Ivan Richard Ugolole <30692799+ugolole@users.noreply.github.com>
2022-05-12 19:48:25 -0400

    Changing a list to a table to display more information.

--- src/components/CoppoLog.jsx
+++ src/components/CoppoLog.jsx
@@ -4,7 +4,7 @@ const UsingFetch = () => {
     const [coppologs, setCoppologs] = useState([])
 
     const fetchData = () => {
-        fetch("http://localhost:8070/CoppoLogs?count=3000")
+        fetch("http://localhost:8070/CoppoLogs?count=5000")
             .then(response => {
                 return response.json()
             })
@@ -18,14 +18,32 @@ const UsingFetch = () => {
     }, [])
 
     return (
-        <div>
-            {coppologs.length > 0 && (
-                <ul>
-                    {coppologs.map(coppolog => (
-                        <li key={coppolog.id}>{coppolog.message}</li>
-                    ))}
-                </ul>
-            )}
+        <div class="centered">
+            <table>
+                <thead>
+                    <tr>
+                        <th>Id</th>
+                        <th>Message</th>
+                        <th>Level</th>
+                        <th>Time stamp</th>
+                        <th>Expection</th>
+                    </tr>
+                </thead>
+                { coppologs.length > 0 && (
+                    <tbody>
+                        {coppologs.map(coppologs => (
+                            <tr>
+                                <td>{coppologs.id}</td>
+                                <td>{coppologs.message}</td>
+                                <td>{coppologs.level}</td>
+                                <td>{coppologs.timeStamp}</td>
+                                <td>{coppologs.exception}</td>
+                            </tr>
+                           
+                        ))}
+                    </tbody>
+                )}
+            </table>
         </div>
     )
 }
